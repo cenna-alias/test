7d2b1e6374bce31c841c7fa07c14e2a85b46f87e
Cenna Alias <cennaalias29@gmail.com>
2026-07-15 11:57:45 +0530

    Delete training directory

--- training/sum_odd_even.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number: "))
-esum = 0
-osum = 0
-for i in range(1, n+1, 2):
-    osum += i
-for i in range(2, n+1, 2):
-    esum += i
-print("Sum of odd numbers from 1 to", n, ":", osum)
-print("Sum of even numbers from 1 to", n, ":", esum)
